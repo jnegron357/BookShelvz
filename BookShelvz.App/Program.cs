cebec2a88e8bd55d72991587b11ebc293e2b699a
Julio Negron <jnegron@nextgen.com>
2020-01-06 14:14:48 -0500

    Added some data to work with

--- BookShelvz.App/Program.cs
+++ BookShelvz.App/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookShelvz.Data;
+using BookShelvz.Domain.Models;
+using System;
 
 namespace BookShelvz.App
 {
@@ -6,7 +8,32 @@ namespace BookShelvz.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using BookShelvzContext context = new BookShelvzContext();
+            //context.ComicBooks.Add(new ComicBook
+            //{
+            //})
+            context.Colorists.Add(new Colorist
+            {
+                FirstName = "Terry",
+                LastName = "Austin"
+            });
+            context.Colorists.Add(new Colorist
+            {
+                FirstName = "John",
+                LastName = "Romita Sr."
+            });
+            context.Colorists.Add(new Colorist
+            {
+                FirstName = "Steve",
+                LastName = "Ditko"
+            });
+            context.Colorists.Add(new Colorist
+            {
+                FirstName = "Frank",
+                LastName = "Miller"
+            });
+            context.SaveChanges();
+            Console.WriteLine("Inkers Added!");
         }
     }
 }
