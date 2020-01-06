6229cd1ed9dff4c960254dbd63934dbf00ec706a
Julio Negron <jnegron@nextgen.com>
2020-01-06 14:31:04 -0500

    Updates to ComicBook object

--- BookShelvz.Domain/Models/ComicBook.cs
+++ BookShelvz.Domain/Models/ComicBook.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using BookShelvz.Domain.Contracts;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookShelvz.Domain.Models
 {
     public class ComicBook : IPublication
     {
         public int Id { get; set; }
+        [ForeignKey("Publisher")]
+        public int PublisherId { get; set; }
+        [Required]
         public DateTime? PublicationDate { get; set; }
         public DateTime? PurchaseDate { get; set; }
-        public Person Author { get; set; }
-        public Publisher Publisher { get; set; }
+        [Required]
         public string Title { get; set; }
         public int? Pages { get; set; }
+        
+        public Publisher Publisher { get; set; }
+        public ICollection<ComicBookCreatorsXref> Creators { get; set; }
     }
 }
